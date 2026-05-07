186f22e1270a2fb93cd5f1fed0d543161ec8253c
Mahendra Singh Choudhary <54597741+mahendra2890@users.noreply.github.com>
2026-05-07 18:13:46 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        index = {nums[0]: 0}
+        
+        for i in range(1, len(nums)):
+            print(index)
+            print("index.get(target-nums[i]): ", index.get(target-nums[i]))
+            if index.get(target-nums[i]) is not None:
+                return [index[target-nums[i]], i]
+            else:
+                index[nums[i]] = i
