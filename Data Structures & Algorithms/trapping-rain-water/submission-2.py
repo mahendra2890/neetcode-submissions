0144a18e83965a6a9079bebb6a8aa33a82ccd203
Mahendra Singh Choudhary <54597741+mahendra2890@users.noreply.github.com>
2026-05-09 16:46:11 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # Arr: [0, 2, 0, 3, 1, 0, 1, 3, 2, 1]
+        # NGE: [1, 3, 3, 7, 7, 7, 7,10,10,10]
+        # PGE: [-1,-1,1, 1, 3, 3, 3, 3, 7, 8]
+        # ans+=[0, 0, 2, 0, 2, 3, 2, 0, 0, 0]
+        NGE = [len(height)-1]*len(height)
+        PGE = [0]*len(height)
+        for i in range(0, len(height), 1):
+            if i == 0 or height[i] >= height[PGE[i-1]]:
+                PGE[i] = i
+            else:
+                PGE[i] = PGE[i-1]
+        NGE = len(height)-1
+        ans = 0
+        for i in range(len(height)-1, 0, -1):
+            if i == len(height)-1 or height[i] >= height[NGE]:
+                NGE = i
+            ans+=(max(0, min(height[PGE[i]], height[NGE])-height[i]))
+
+        # print(height)
+        # print(NGE)
+        # print(PGE)
+        # for i in range(0, len(height), 1):
+            
+        return ans
+
+        
